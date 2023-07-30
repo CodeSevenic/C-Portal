790db6c376840570d17c6d4f9713533cee192ccd
CodeSevenic <sevenicbookings@gmail.com>
2023-07-30 16:30:57 +0200

    Hub-Nabled

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,30 +2,6 @@
 
 console.log('process.env.HUBSPOT_API_KEY: ', process.env.HUBSPOT_API_KEY);
 
-exports.getTickets = async (req, res) => {
-  console.log('Request has been made to get tickets');
-  try {
-    const response = await axios.get('https://api.hubapi.com/crm/v3/objects/tickets', {
-      params: {
-        limit: 100,
-        archived: false,
-      },
-      headers: { Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}` },
-    });
-
-    // Filter the tickets that belong to 'support_pipeline'
-    const supportTickets = response.data.results.filter(
-      (ticket) => ticket.properties.hs_pipeline === '46085271'
-    );
-
-    console.log('Filtered support tickets: ', supportTickets);
-    res.send({ results: supportTickets });
-  } catch (error) {
-    console.error('Error fetching tickets: ', error.message);
-    res.status(500).send(error);
-  }
-};
-
 exports.getTicketsByContactEmail = async (req, res) => {
   const contactEmail = 'sibusiso@mo.agency'; // Hardcoded for testing
   console.log(`Request has been made to get tickets associated with contact ${contactEmail}`);
@@ -100,21 +76,6 @@ exports.getTicketsByContactEmail = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
-  }
-};
-
-const getTicketById = async (ticketId) => {
-  try {
-    const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/tickets/${ticketId}`, {
-      headers: { Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}` },
-    });
-
-    console.log('Ticket by ID: ', response.data);
-
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
   }
 };
 
