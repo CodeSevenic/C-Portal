eb59feca9db6a71636874b46d1c8dbdf45f7cbcd
CodeSevenic <sevenicbookings@gmail.com>
2023-07-30 08:08:08 +0200

    Hub-Nabled

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,10 +2,10 @@
 const { getTickets, createTicket, updateTicket } = require('../controllers/tickets');
 const router = express.Router();
 
-app.get('/tickets', getTickets);
+router.get('/tickets', getTickets);
 
-app.post('/tickets', createTicket);
+router.post('/tickets', createTicket);
 
-app.put('/tickets/:id', updateTicket);
+router.put('/tickets/:id', updateTicket);
 
 module.exports = router;
