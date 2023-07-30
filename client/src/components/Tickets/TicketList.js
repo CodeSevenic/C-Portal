7dba730796e817b12025d6dfd7f96c11557ebfbd
CodeSevenic <sevenicbookings@gmail.com>
2023-07-30 08:31:45 +0200

    Hub-Nabled

--- client/src/components/Tickets/TicketList.js
+++ client/src/components/Tickets/TicketList.js
@@ -36,19 +36,23 @@ function Ticketing() {
     resetTicketToEdit();
     fetchTickets();
   };
-
+  console.log('Tickets: ', tickets);
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-1 bg-white shadow rounded p-4">
           <h1 className="text-2xl mb-4">Tickets</h1>
-          {tickets.map((ticket) => (
-            <div key={ticket.id} className="mb-2">
-              <button className="text-blue-500" onClick={() => editTicket(ticket)}>
-                {ticket.properties.title}
-              </button>
-            </div>
-          ))}
+          {/* {tickets.length > 0 &&
+            tickets.map((ticket) => {
+              console.log('ticket: ', ticket.properties.subject);
+              return (
+                <div key={ticket.id} className="mb-2">
+                  <button className="text-blue-500" onClick={() => editTicket(ticket)}>
+                    {ticket.properties.subject}
+                  </button>
+                </div>
+              );
+            })} */}
           <button className="bg-blue-500 text-white rounded p-2 mt-4" onClick={resetTicketToEdit}>
             Create a new ticket
           </button>
