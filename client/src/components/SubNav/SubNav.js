f4280da06f80d252e573366f742f75073a5c3a7f
CodeSevenic <sevenicbookings@gmail.com>
2023-08-02 06:23:53 +0200

    C Portal Code update

--- client/src/components/SubNav/SubNav.js
+++ client/src/components/SubNav/SubNav.js
@@ -12,27 +12,27 @@ const SubNav = () => {
     <div className="nav-items max-w-6xl mx-auto px-5 pt-20">
       <ul className="flex gap-9 justify-around">
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col items-center text-xl">
+          <NavLink className="nav-link font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col items-center text-xl">
             <MdOutlineSendTimeExtension className="text-5xl text-moBlue  block" />
             <span className="text-gray-500 ">Create a ticket</span>
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5  flex flex-col  items-center text-xl">
+          <NavLink className="nav-link font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5  flex flex-col  items-center text-xl">
             <BiCheckDouble className="text-5xl text-moBlue  block" />
             <span className="text-gray-500">View tickets</span>
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col  text-gray-500 items-center text-xl">
+          <NavLink className="nav-link font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col  text-gray-500 items-center text-xl">
             <GrArticle className="text-5xl  text-moBlue block" />
             <span className="text-gray-500">Resources</span>
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex  flex-col text-gray-500 items-center text-xl">
+          <NavLink className="nav-link font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex  flex-col text-gray-500 items-center text-xl">
             <MdOutlineVideoLibrary className="text-5xl  text-moBlue block" />
-            Webinars
+            <span className="text-gray-500">Webinars</span>
           </NavLink>
         </li>
       </ul>
