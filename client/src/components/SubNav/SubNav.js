e5a22d8eef1b76c9440ec70613f2c7cbe2a8bdd9
CodeSevenic <sevenicbookings@gmail.com>
2023-08-02 06:21:28 +0200

    C Portal Code update

--- client/src/components/SubNav/SubNav.js
+++ client/src/components/SubNav/SubNav.js
@@ -12,26 +12,26 @@ const SubNav = () => {
     <div className="nav-items max-w-6xl mx-auto px-5 pt-20">
       <ul className="flex gap-9 justify-around">
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex hover:!text-moBlueLight transition-all duration-300 flex-col text-moBlue items-center text-xl">
-            <MdOutlineSendTimeExtension className="text-5xl block" />
-            <span className="text-gray-500">Create a ticket</span>
+          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col items-center text-xl">
+            <MdOutlineSendTimeExtension className="text-5xl text-moBlue  block" />
+            <span className="text-gray-500 ">Create a ticket</span>
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 hover:text-moBlueLight transition-all duration-300 flex flex-col text-gray-500 items-center text-xl">
-            <BiCheckDouble className="text-5xl text-moBlue hover:text-moBlueLight transition-all duration-300 block" />
-            View tickets
+          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5  flex flex-col  items-center text-xl">
+            <BiCheckDouble className="text-5xl text-moBlue  block" />
+            <span className="text-gray-500">View tickets</span>
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col hover:text-moBlueLight transition-all duration-300 text-gray-500 items-center text-xl">
-            <GrArticle className="text-5xl hover:text-moBlueLight transition-all duration-300 text-moBlue block" />
+          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex flex-col  text-gray-500 items-center text-xl">
+            <GrArticle className="text-5xl  text-moBlue block" />
             Resources
           </NavLink>
         </li>
         <li>
-          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex hover:text-moBlueLight transition-all duration-300 flex-col text-gray-500 items-center text-xl">
-            <MdOutlineVideoLibrary className="text-5xl hover:text-moBlueLight transition-all duration-300 text-moBlue block" />
+          <NavLink className="font-semibold border-1 border-gray-50 shadow-md rounded-xl px-10 min-w-[200px] py-5 flex  flex-col text-gray-500 items-center text-xl">
+            <MdOutlineVideoLibrary className="text-5xl  text-moBlue block" />
             Webinars
           </NavLink>
         </li>
