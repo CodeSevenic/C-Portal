da6d54a0ca92ec3ffc8a005d05e6151530822b02
CodeSevenic <sevenicbookings@gmail.com>
2023-08-04 12:49:07 +0200

    C Portal Code update

---
+++ client/src/components/Webinars/Webinars.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Webinars = () => {
+  return (
+    <div className="py-20">
+      <h1 className="text-8xl font-bold">Webinars Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Webinars;
