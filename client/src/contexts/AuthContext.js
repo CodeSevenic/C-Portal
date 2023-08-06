029f30f53dffb72328986fa29508e98735a44498
CodeSevenic <sevenicbookings@gmail.com>
2023-08-06 09:25:13 +0200

    C Portal Code update

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -5,24 +5,8 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [hubSpotIds, setHubSpotIds] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false);
   const [userData, setUserData] = useState({});
-  const [deletingAccount, setDeletingAccount] = useState(false);
-
-  const [authAccountDeleted, setAuthAccountDeleted] = useState(false);
-
-  function getHubSpotIds(data) {
-    let names = [];
-    for (let name in data.appAuths) {
-      names.push(name);
-    }
-    sessionStorage.setItem('hubSpotIds', JSON.stringify(names));
-
-    setHubSpotIds(names);
-
-    return names;
-  }
 
   const login = async (email, password) => {
     console.log('Login: email: ', email);
@@ -54,8 +38,6 @@ export const AuthContextProvider = ({ children }) => {
         sessionStorage.setItem('isAdmin', data.isAdmin);
         sessionStorage.setItem('username', data.username);
         sessionStorage.setItem('isLoggedIn', data.isLoggedIn);
-
-        getHubSpotIds(data);
 
         setIsLoggedIn(true);
 
@@ -100,8 +82,6 @@ export const AuthContextProvider = ({ children }) => {
       sessionStorage.setItem('username', data.username);
       sessionStorage.setItem('isLoggedIn', data.isLoggedIn);
 
-      getHubSpotIds(data);
-
       console.log('Register: Successfully went through!!!');
       return data;
     } else {
@@ -110,70 +90,17 @@ export const AuthContextProvider = ({ children }) => {
     }
   };
 
-  const installApp = async (appId) => {
-    const response = await fetch('http://localhost:8000/api/install', {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ appId }),
-    });
-
-    if (!response.ok) {
-      throw new Error('App installation failed');
-    }
-  };
-
-  const handleDeleteAccount = async (userId, portalId) => {
-    setDeletingAccount(true);
-    try {
-      const res = await fetch(`${baseURL}/api/delete-account-auth/${userId}/${portalId}`, {
-        method: 'POST', // Specify the method
-        headers: {
-          'Content-Type': 'application/json', // Set the content type to be JSON
-        },
-        credentials: 'include', // Include credentials for sessions
-      });
-
-      if (!res.ok) {
-        setDeletingAccount(false);
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-      if (res.ok) {
-        setDeletingAccount(false);
-        setAuthAccountDeleted(true);
-
-        setAuthAccountDeleted(true);
-
-        // after 5 seconds, set authAccountDeleted back to false
-        setTimeout(() => {
-          setAuthAccountDeleted(false);
-        }, 5000);
-
-        return res.json(); // Parse the JSON response
-      }
-    } catch (error) {
-      console.log(error); // Catch and log any errors
-    }
-  };
-
   return (
     <AuthContext.Provider
       value={{
         login,
         register,
-        installApp,
         isAdmin,
         isLoggedIn,
         setIsAdmin,
-        hubSpotIds,
-        setHubSpotIds,
         setIsLoggedIn,
         userData,
         setUserData,
-        authAccountDeleted,
-        setAuthAccountDeleted,
-        handleDeleteAccount,
-        deletingAccount,
       }}
     >
       {children}
